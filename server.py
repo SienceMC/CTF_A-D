fccc598bb7c72f9972ae1702d64bd01e7e21f5fb
SienceMC <41262691+SienceMC@users.noreply.github.com>
2020-02-26 21:44:00 +0100

    oof mir si müed

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 import flask
-from flask import render_template
+from flask import render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 # import sqlite3
 from datetime import datetime
 # import os
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ctf.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ctf.db'
 db = SQLAlchemy(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teams.db'
 # teams = SQLAlchemy(app)
@@ -28,56 +28,102 @@ class Flags(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     flag = db.Column(db.String(200))
     points = db.Column(db.Integer)
-    team1 = db.Column(db.Boolean)
-    team2 = db.Column(db.Boolean)
-
+    teams = db.Column(db.String(1000), default="")
+    
     def __repr__(self):
-        return '<Task %r>' % self.id
-
+        return '<Flag %r>' % self.id
 
 class Teams(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(5))
-    points = db.Column(db.Integer)
+    points = db.Column(db.Integer, default=0)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Team %r>' % self.id
 
 # db.create_all()
 
 @app.route("/")
 def menu():
-    return render_template('index.html')
+    return render_template('index.html', teams=Teams.query.all())
 
-
-@app.route("/6C02828918114740F1A100B38EEB78B1396F80EEED6AA98EF2222D9BAD6BC158")
-def team1():
-    return render_template('team_1.html')
-
-
-@app.route("/75490bd7b93e6fa7d18cfdea90cc6bcb983d5f3ea326249d2709ca6c94bc07ba", methods=["POST"])
+@app.route("/capture", methods=["POST"])
 def submit():
     flag = flask.request.form.get("Flag")
     team = flask.request.form.get("Team")
-    if int(team) == 1:
-        flagsthatarethesameastheeingegebeneflaginputimfau = Flags.query.filter_by(
-            flag=flag).all()
-
-        # flaggedflags = flagthatshit("team1")
-    elif int(team) == 2:
-        pass
+    flaged = Flags.query.filter_by(flag = flag).all()
+    if not team.name in flaged.teams:
+        try:
+            db.session.delete(flaged)
+            flaged.teams += team.name
+            db.session.remove(team)
+            team.points += flaged.points
+            db.session.add(team)
+            db.session.add(flaged)
+            db.session.commit()
+        except:
+            return "<h1>OH CRAP!</h1><h2>There was an error occured!</h2>"
+        finally:
+            return f"<h1>SUBMITTED!</h1><h2>YOUR TEAMS' SCORE's now: {team.points}</h2>"
+    elif flaged.team1:
+        return "<h1>YOU LITTLE...</h1>"
+    # flaggedflags = flagthatshit("team1")
+        
         # flaggedflags = flagthatshit("team2")
-    return flagsthatarethesameastheeingegebeneflaginputimfau
+    return str(flaged)
 
 
-@app.route("/2C57B47A189B1BD3944A039FCD7EC6D872F2BFB3FEDA68A6BFE3DD326F650A64")
-def team2():
-    return render_template('team_2.html')
+@app.route("/team/<int:id>", methods=['GET'])
+def team(id):
+    return render_template('team.html', team=Teams.query.filter_by(id=id).first())
 
 
-@app.route("/nimda")
+@app.route("/nimda", methods=['GET', 'POST'])
 def admin():
-    return render_template('admin.html')
+    if flask.request.method == "POST":
+        if flask.request.form.get("request") == "teamadd":
+            newteam = Teams(name=f"team{len(Teams.query.all())+1}")
+            db.session.add(newteam)
+            db.session.commit()
+        elif flask.request.form.get("request") == "flagadd":
+            print("Flagadd recived")
+            newflag = Flags(flag=flask.request.form.get("flag"))
+            newflag.points = flask.request.form.get("points")
+            db.session.add(newflag)
+            db.session.commit()
+        # elif flask.request.form.get("request") == "teamdelete":
+        #     team = Teams.query.filter_by(id=flask.request.form.get("team")).first()
+        #     print(team)
+        #     db.session.remove(team)
+        #     db.session.commit()
+        elif flask.request.form.get("request") == "flagdelete":
+            flag = Flags.query.filter_by(id = flask.request.form.get("flag")).all()
+            db.session.remove(flag)
+            db.session.commit()
+    return render_template('admin.html', teams=Teams.query.all(), flags=Flags.query.all())
+
+@app.route('/deleteteam/<int:id>')
+def delete(id):
+    team_to_delete = Teams.query.get_or_404(id)
+
+    try:
+        db.session.delete(team_to_delete)
+        db.session.commit()
+        return redirect('/nimda')
+    except:
+        return 'There was a problem deleting that Team'
+
+@app.route('/deleteflag/<int:id>')
+def delete_(id):
+    flag_to_delete = Flags.query.get_or_404(id)
+
+    try:
+        db.session.delete(flag_to_delete)
+        db.session.commit()
+        return redirect('/nimda')
+    except:
+        return 'There was a problem deleting that Flag'
+
 
 
 if __name__ == "__main__":
