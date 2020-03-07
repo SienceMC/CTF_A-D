ccaaf2afe309901be9206ef848a5a592ad08a031
Silvan Kohler <edu.silvan.kohler@gmail.com>
2020-03-07 15:51:25 +0100

    good changes :airplane:

--- server.py
+++ server.py
@@ -5,12 +5,12 @@ from datetime import datetime
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ctf.db'
 db = SQLAlchemy(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teams.db'
-# teams = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persons.db'
+# persons = SQLAlchemy(app)
 # db = sqlite3.connect("ctf.db")
 # cursor = db.cursor()
-# def flagthatshit(team):
-#     flaggedflags = cursor.execute(f"""SELECT * FROM teams WHERE name="{team}" """)
+# def flagthatshit(person):
+#     flaggedflags = cursor.execute(f"""SELECT * FROM persons WHERE name="{person}" """)
 # Not Working for some Reason:
 
 # Database.execute("""CREATE TABLE flags (
@@ -26,7 +26,7 @@ class Flags(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     flag = db.Column(db.String(200))
     points = db.Column(db.Integer)
-    teams = db.Column(db.String(1000), default="")
+    persons = db.Column(db.String(1000), default="")
     
     def __repr__(self):
         return '<Flag %r>' % self.id
@@ -42,83 +42,96 @@ class Persons(db.Model):
 # db.create_all()
 
 @app.route("/")
-def menu():
-    return render_template('index.html', teams=Teams.query.all())
+def index():
+    ranks = []
+    people = {}
+    for person in Persons.query.all():
+        people.update({person.name: person.points})
+    people = sorted(people.items(), key=lambda item: item[1], reverse=True)
+    for rank, (name, points) in enumerate(people):
+        ranks.append([rank+1, name, points])
+    return render_template('index.html', ranks=ranks)
 
 @app.route("/capture", methods=["POST"])
 def submit():
     flag = flask.request.form.get("Flag")
-    team = flask.request.form.get("Team")
-    team = Teams.query.filter_by(name = team).first()
+    person = flask.request.form.get("Person")
+    print(person)
+    person = Persons.query.filter_by(name = person).first()
     flaged = Flags.query.filter_by(flag = flag).first()
     try:
-        if team.name not in flaged.teams:
+        if person.name not in flaged.persons:
             try:
-                db.session.delete(team)
+                db.session.delete(person)
                 db.session.delete(flaged)
-                flaged.teams += team.name
-                team.points += flaged.points
-                db.session.add(team)
+                flaged.persons += person.name
+                person.points += flaged.points
+                db.session.add(person)
                 db.session.add(flaged)
                 db.session.commit()
             except:
                 return "<h1>OH CRAP!</h1><h2>An error occured!</h2>"
             finally:
-                print(f"INFO: Flagsubmitt recived from Team {team.id}")
-                return f"<h1>SUBMITTED!</h1><h2>YOUR TEAMS' SCORE's now: {team.points}</h2>"
-        elif team.name in flaged.teams:
-            print(f"INFO: Detected multiple submission from team { team.id }")
+                print(f"INFO: Flagsubmit recived from {person.name}")
+                return f"<h1>SUBMITTED!</h1><h2>YOUR SCORE's now: {person.points}</h2>"
+        elif person.name in flaged.persons:
+            print(f"INFO: Detected multiple submission from { person.name }")
             return "<h1>YOU LITTLE...</h1><h2>You really tried to enter a flag two times?</h2>"
     except:
-        print(f"INFO: False Flagsubmitt recived from Team {team.id}")
+        print(f"INFO: False Flagsubmit recived from {person.name}")
         return render_template('false_flag.html')
-    # flaggedflags = flagthatshit("team1")
+    # flaggedflags = flagthatshit("person1")
         
-        # flaggedflags = flagthatshit("team2")
+        # flaggedflags = flagthatshit("person2")
     return str(flaged)
 
 
-@app.route("/team/<int:id>", methods=['GET'])
-def team(id):
-    return render_template('team.html', team=Teams.query.filter_by(id=id).first())
+@app.route("/person", methods=['POST'])
+def person():
+    name = flask.request.form.get("name")
+    print(name)
+    print(Persons.query.filter_by(name=name).first())
+    if Persons.query.filter_by(name=name).first() == None:
+        newperson = Persons(name=name)
+        db.session.add(newperson)
+        db.session.commit()
+        print("INFO: Person added")
+    return render_template('person.html', person=Persons.query.filter_by(name=name).first())
 
 
 @app.route("/nimda", methods=['GET', 'POST'])
 def admin():
-    if flask.request.method == "POST":
-        if flask.request.form.get("request") == "teamadd":
-            newteam = Teams(name=f"team{len(Teams.query.all())+1}")
-            db.session.add(newteam)
-            db.session.commit()
-            print("INFO: Teamadd recived")
-        elif flask.request.form.get("request") == "flagadd":
+    if flask.request.method == "POST":  
+        if flask.request.form.get("request") == "flagadd":
             print("INFO: Flagadd recived")
             newflag = Flags(flag=flask.request.form.get("flag"))
             newflag.points = flask.request.form.get("points")
             db.session.add(newflag)
             db.session.commit()
-        # elif flask.request.form.get("request") == "teamdelete":
-        #     team = Teams.query.filter_by(id=flask.request.form.get("team")).first()
-        #     print(team)
-        #     db.session.remove(team)
+        # elif flask.request.form.get("request") == "persondelete":
+        #     person = Persons.query.filter_by(id=flask.request.form.get("person")).first()
+        #     print(person)
+        #     db.session.remove(person)
         #     db.session.commit()
         elif flask.request.form.get("request") == "flagdelete":
             flag = Flags.query.filter_by(id = flask.request.form.get("flag")).all()
             db.session.remove(flag)
             db.session.commit()
             print("INFO: Flagdelete recived")
-    return render_template('admin.html', teams=Teams.query.all(), flags=Flags.query.all())
+        return redirect("/nimda")
+    else:
+        return render_template('admin.html', persons=Persons.query.all(), flags=Flags.query.all())
 
-@app.route('/deleteteam/<int:id>')
-def delete(id):
-    team_to_delete = Teams.query.get_or_404(id)
+@app.route('/deleteperson/<name>')
+def delete(name):
+    person_to_delete = Persons.query.filter_by(name=name).first()
 
     try:
-        db.session.delete(team_to_delete)
+        db.session.delete(person_to_delete)
         db.session.commit()
         return redirect('/nimda')
     except:
-        return 'There was a problem deleting that Team'
+        return 'There was a problem deleting that Person'
 
 @app.route('/deleteflag/<int:id>')
 def delete_(id):
